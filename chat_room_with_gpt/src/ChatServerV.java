49495b8288144a9fc7db077a5d10ee8a54f78dec
kartikey <dwivedi.k@northeastern.edu>
2023-04-24 19:03:28 -0400

    delete records from clients and messages table

--- chat_room_with_gpt/src/ChatServerV.java
+++ chat_room_with_gpt/src/ChatServerV.java
@@ -2,6 +2,11 @@ import java.io.Serializable;
 import java.rmi.*;
 import java.rmi.server.*;
 import java.rmi.registry.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.*;
 
 public class ChatServerV extends UnicastRemoteObject implements ChatServerInterface, Serializable  {
@@ -81,9 +86,28 @@ public class ChatServerV extends UnicastRemoteObject implements ChatServerInterf
             Registry registry = LocateRegistry.createRegistry(3000);
             registry.rebind("ChatServer", server);
             System.out.println("Chat server started");
+            deleteDatabase("jdbc:mysql://localhost:3306/db1");
+            deleteDatabase("jdbc:mysql://localhost:3306/db2");
         } catch (Exception e) {
             System.err.println("Chat server exception: " + e.getMessage());
             e.printStackTrace();
         }
+    }
+
+    private static void deleteDatabase(String url) {
+            String user = "root";
+            String password = "";
+            String sql1 = "DELETE FROM messages";
+            String sql2 = "DELETE FROM clients";
+            try (Connection conn = DriverManager.getConnection(url, user, password);
+                 Statement stmt = conn.createStatement()) {
+                int rowsAffectedInMessages = stmt.executeUpdate(sql1);
+                System.out.println(rowsAffectedInMessages + " rows deleted from the messages table.");
+                int rowsAffectedInClients = stmt.executeUpdate(sql2);
+                System.out.println(rowsAffectedInClients + " rows deleted from the client table.");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
     }
 }
